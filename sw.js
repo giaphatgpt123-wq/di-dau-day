83ec9dd42f94e3f0b0c61df4feb659c172837609
giaphatgpt123-wq <giaphatgpt123@gmail.com>
2026-09-11 11:17:40 +0700

    Finalize RC1.2 offline app shell

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_VERSION='d1-rc1.2-shell-v6';
 const RUNTIME_CACHE='d1-rc1.2-runtime-v6';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_VERSION).then(c=>c.addAll(APP_SHELL))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_VERSION&&k!==RUNTIME_CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
